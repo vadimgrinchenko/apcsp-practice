cac1b5fbd96808dbf04156e8ecfca0637aac55ba
smurfer <vadim.grinchenko29@student.tamdistrict.org>
2026-08-27 20:44:38 +0000

    second commit

---
+++ topic-3-4/sequence-practice.py
@@ -0,0 +1,12 @@
+scores = [77 , 85, 91, 68, 88]
+title = "weekly report"
+print(scores[0])
+print(scores[2])
+print(scores[4])
+scores[2] = 86
+scores.append(93)
+print(title[0:6])
+print(title[7:13])
+label = str(scores) + str(title)
+print(label)
+#strings can be changed, but lists are permanent and cannot be

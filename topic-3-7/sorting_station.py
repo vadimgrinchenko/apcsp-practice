da6320053ceaf8470a7f87f395cbd846d42afb7e
smurfer <vadim.grinchenko29@student.tamdistrict.org>
2026-09-17 20:52:40 +0000

    sorter

---
+++ topic-3-7/sorting_station.py
@@ -0,0 +1,27 @@
+productinfo = input("Product Code: ")
+destination = 0
+
+shape = productinfo[0:4]   
+color = productinfo[4:7]    
+size = int(productinfo[7:10])   
+mass = int(productinfo[10:14])
+condition = productinfo[14:15]
+
+if condition == "D" or mass > 2000 or size > 50:
+    destination = "Inspect"
+else:
+    if shape == "BALL":
+        if size > 10 and color == "RED":
+            destination = "B"
+        else:
+            destination = "A"   
+    else:
+        if shape == "CUBE":
+            if color == "GRN" or color == "BLU" and size < 10:
+                destination = "C"
+            else:
+                destination = "D"
+        else:
+            destination = "E"
+print(destination)
+
